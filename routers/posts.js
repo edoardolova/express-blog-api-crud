631068c302bf61d5257903875ec7f78ab9aa7bef
Edoardo Lova <edoardolova81@gmail.com>
2025-07-01 13:04:37 +0200

    install express + use express static +  add crud index + show + destroy

---
+++ routers/posts.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const router = express.Router();
+const posts = require("../data/postsData.js")
+
+//index
+router.get("/", (req,res) =>{
+    res.json(posts);
+});
+
+//show
+router.get("/:id", (req,res) =>{
+    const id = req.params.id;
+    const post = posts.find(post => post.id === Number(id));
+    if(post){
+        return res.json(post);
+    }
+    else{
+        return res.status(404).json({err: "post not found"});
+    }
+});
+
+// store
+router.post("/", (req, res) =>{
+    res.send("aggiungo un nuovo post");
+});
+
+// update
+router.put("/:id", (req, res) =>{
+    const id = req.params.id;
+    res.send(`cambio interamente il post con id: ${id}`);
+});
+
+//modify
+router.patch("/:id", (req, res) =>{
+    const id = req.params.id;
+    res.send(`cambio in parte il post con id: ${id}`);
+});
+
+// detroy
+router.delete("/:id", (req, res) =>{
+    const id = Number(req.params.id);
+    const post = posts.find(post => post.id === id);
+    if (post) {
+        posts.splice(posts.indexOf(post),1);
+    }
+    else{
+        res.status(404);
+        return res.json({
+            status: 404,
+            err: "not found",
+            mess: "post not found"
+        })
+    }
+    console.log(posts);
+    res.sendStatus(204);
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 5500;
+const postsRouter = require("./routers/posts.js");
+
+
+
+app.listen(port, ()=>{
+    console.log(`listening at port: ${port}`);
+});
+
+app.use(express.static(`public`));
+
+app.get("/", (req, res)=>{
+    res.send("Benvenuti sul mio blog");
+})
+
+app.use("/posts", postsRouter);
